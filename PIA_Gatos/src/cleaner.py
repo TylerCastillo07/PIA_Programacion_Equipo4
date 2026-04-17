98ee1440355703b8923b4db04d43a01d388169f3
Monserrat Cárdenas <monserrat.cardenasr@uanl.edu.mx>
2026-04-16 22:39:27 -0600

    Implement data cleaning function in cleaner.py
    Added a function to clean and extract data from input.

--- PIA_Gatos/src/cleaner.py
+++ PIA_Gatos/src/cleaner.py
@@ -1,1 +1,18 @@
+import re
 
+def limpiar_datos(data):
+    
+    peso_texto = data.get("weight", {}).get("metric", "0")
+    numeros = re.findall(r"\d+", peso_texto)
+    peso_final = int(numeros[0]) if numeros else 0
+
+    
+    clean = {
+        "id": data.get("id"),
+        "raza": data.get("name", "").capitalize(),
+        "origen": data.get("origin", "Desconocido"),
+        "inteligencia": int(data.get("intelligence", 0)),
+        "energia": int(data.get("energy_level", 0)),
+        "peso_min_kg": peso_final
+    }
+    return clean
