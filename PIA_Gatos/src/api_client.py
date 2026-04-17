56674cc369933e3524581387a9f1e3344f34f57c
AmericaTamayo <america.vazquezt@uanl.edu.mx>
2026-04-16 22:09:56 -0600

    Update api_client.py

--- PIA_Gatos/src/api_client.py
+++ PIA_Gatos/src/api_client.py
@@ -1,1 +1,13 @@
+import requests
 
+def get_data(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error en API: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Error de conexión: {e}")
+        return None
