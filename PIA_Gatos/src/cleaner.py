eef36f11708df847e02a8ee3544d853a191a436a
TylerCastillo07 <tyler.castillol@uanl.edu.mx>
2026-04-18 14:46:34 -0600

    Update cleaner.py

--- PIA_Gatos/src/cleaner.py
+++ PIA_Gatos/src/cleaner.py
@@ -1,18 +1,21 @@
 import re
 
 def limpiar_datos(data):
+    # Manejo de nulos: si no existe el campo, ponemos un valor por defecto
+    nombre = data.get("name", "Desconocido").capitalize()
     
-    peso_texto = data.get("weight", {}).get("metric", "0")
-    numeros = re.findall(r"\d+", peso_texto)
-    peso_final = int(numeros[0]) if numeros else 0
+    # Aplanamiento y extracción de peso
+    peso_raw = data.get("weight", {}).get("metric", "0")
+    numeros = re.findall(r"\d+", peso_raw)
+    peso_min = int(numeros[0]) if numeros else 0
 
     
-    clean = {
-        "id": data.get("id"),
-        "raza": data.get("name", "").capitalize(),
+    return {
+        "id": data.get("id", "n/a"),
+        "raza": nombre,
         "origen": data.get("origin", "Desconocido"),
         "inteligencia": int(data.get("intelligence", 0)),
         "energia": int(data.get("energy_level", 0)),
-        "peso_min_kg": peso_final
+        "peso_min_kg": peso_min
     }
-    return clean
+
