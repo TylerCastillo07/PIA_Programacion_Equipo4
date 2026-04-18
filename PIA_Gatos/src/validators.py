094ef6670e6d6065a8cfe6a99324799f2204bac2
TylerCastillo07 <tyler.castillol@uanl.edu.mx>
2026-04-18 14:45:53 -0600

    Update validators.py

--- PIA_Gatos/src/validators.py
+++ PIA_Gatos/src/validators.py
@@ -1,7 +1,14 @@
 import re
 
-def validar_nombre(nombre):
-    return bool(re.search(r"^[A-Za-z\s]+$", nombre))
-
 def validar_id(id_valor):
-    return len(str(id_valor)) == 4
+    # El ID siempre son 4 letras minúsculas
+    return bool(re.search(r"^[a-z]{4}$", str(id_valor)))
+
+def validar_nombre(nombre):
+    # Agregamos "-" para nombres como "American-Bobtail"
+    return bool(re.search(r"^[A-Za-z\s\-]+$", str(nombre)))
+
+def validar_inteligencia(valor):
+    #Aseguramos que sea 1-5
+    return bool(re.search(r"^[1-5]$", str(valor)))
+
