bd97ffe57653aa241b27ce85a99333b90b8afb24
TylerCastillo07 <tyler.castillol@uanl.edu.mx>
2026-05-10 17:55:32 -0600

    Update cleaner.py

--- PIA_Gatos/src/cleaner.py
+++ PIA_Gatos/src/cleaner.py
@@ -2,16 +2,13 @@ import re
 
 def limpiar_datos(gato):
     try:
-        
         peso_crudo = gato.get("weight", {}).get("metric", "0")
         numeros = re.findall(r'\d+', peso_crudo)
-        
         try:
-            peso_kg = int(numeros[0]) if numeros else 0
-        except (ValueError, IndexError):
+            peso_kg = sum(map(int, numeros)) / len(numeros) if numeros else 0
+        except:
             peso_kg = 0
 
-        
         datos_limpios = {
             "id": gato.get("id", "N/A"),
             "nombre": gato.get("name", "Sin nombre").strip(),
@@ -25,4 +22,5 @@ def limpiar_datos(gato):
     except Exception as e:
         print(f"Error procesando el gato {gato.get('name')}: {e}")
         return None
+
 
